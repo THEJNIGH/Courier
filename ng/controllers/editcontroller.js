a3da4bb6f18b89922ac474dd57554511b5aaef43
jacobnigh <jacobnigh@hotmail.com>
2016-03-23 17:22:59 -0400

    Made the user able to delete their account

--- ng/controllers/editcontroller.js
+++ ng/controllers/editcontroller.js
@@ -1,6 +1,7 @@
 angular.module('courier')
-  .controller('EditController', function($scope, $firebaseAuth, $rootScope, $firebaseObject) {
+  .controller('EditController', function($scope, $firebaseAuth, $rootScope, $firebaseObject, $location) {
     $rootScope.css = 'edit';
+    $scope.del = false;
 
     var obj;
     var URL = 'https://courier-app.firebaseio.com/';
@@ -11,7 +12,6 @@ angular.module('courier')
     $scope.authObj.$onAuth(function(authData) {
       if(authData) {
         var userRef = new Firebase(URL + '/users/' + authData.uid);
-
         obj = $firebaseObject(userRef);
         obj.$loaded()
         .then(function(data) {
@@ -30,10 +30,24 @@ angular.module('courier')
     });// ends $onAuth
 
     $scope.editProfile = function() {
-      console.log("Saved successfully.");
       $scope.user.name = $scope.editUser.name;
       $scope.user.email = $scope.editUser.email;
       $scope.user.bio = $scope.editUser.bio;
       obj.$save();
+      alert('Saved successfully');
+      $location.path('/chat');
+    }
+
+    $scope.delUser = function() {
+      $scope.authObj.$removeUser({
+        email: $scope.user.email,
+        password: $scope.passUser
+      }).then(function() {
+        obj.$remove();
+        alert('Account removed successfully!');
+        $location.path('/')
+      }).catch(function(error) {
+        console.error("Error: ", error);
+      });
     }
   })
--- www/js/main.js
+++ www/js/main.js
@@ -117,8 +117,9 @@ angular.module('courier')
   }]);
 
 angular.module('courier')
-  .controller('EditController', ["$scope", "$firebaseAuth", "$rootScope", "$firebaseObject", function($scope, $firebaseAuth, $rootScope, $firebaseObject) {
+  .controller('EditController', ["$scope", "$firebaseAuth", "$rootScope", "$firebaseObject", "$location", function($scope, $firebaseAuth, $rootScope, $firebaseObject, $location) {
     $rootScope.css = 'edit';
+    $scope.del = false;
 
     var obj;
     var URL = 'https://courier-app.firebaseio.com/';
@@ -129,7 +130,6 @@ angular.module('courier')
     $scope.authObj.$onAuth(function(authData) {
       if(authData) {
         var userRef = new Firebase(URL + '/users/' + authData.uid);
-
         obj = $firebaseObject(userRef);
         obj.$loaded()
         .then(function(data) {
@@ -148,11 +148,25 @@ angular.module('courier')
     });// ends $onAuth
 
     $scope.editProfile = function() {
-      console.log("Saved successfully.");
       $scope.user.name = $scope.editUser.name;
       $scope.user.email = $scope.editUser.email;
       $scope.user.bio = $scope.editUser.bio;
       obj.$save();
+      alert('Saved successfully');
+      $location.path('/chat');
+    }
+
+    $scope.delUser = function() {
+      $scope.authObj.$removeUser({
+        email: $scope.user.email,
+        password: $scope.passUser
+      }).then(function() {
+        obj.$remove();
+        alert('Account removed successfully!');
+        $location.path('/')
+      }).catch(function(error) {
+        console.error("Error: ", error);
+      });
     }
   }])
 
